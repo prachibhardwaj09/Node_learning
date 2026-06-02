0dbe5d8563cdf982902e74d6a4e21000ba872242
prachi_bhardwaj <prachipihu09@gmail.com>
2026-06-02 12:55:13 +0800

    initial commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+// GLOBALS - NO WINDOW !!!!
+// __dirname - Path to current Directory
+// require = function to use modules (CommonJS)
+// module - info about current module(file)
+// process - info about env where the program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+    console.log("hello world");
+}, 1000);
---
+++ 3-module.js
@@ -0,0 +1,12 @@
+const names = require('./4-names');//exporting something
+const sayHi = require('./5-Utlis');
+const data = require('./6-alternatives-flavor');
+require('./7-mind-grenade')//it loads the file runs it once and executes add values() and print output
+// console.log(data);
+
+// console.log(names);
+
+
+// sayHi('pihu')
+// sayHi(names.john)
+// sayHi(names.peter) 
